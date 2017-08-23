e3d5971852d37b03f30376d2738a400dedfa2a1b
matt-jhn <bjohnsonmatt@gmail.com>
2017-08-22 23:13:51 -0400

    Added route to redirect from root page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root :to => redirect('/combatants')
+
   get '/combatants' => 'combatants#index'
   get 'combatants/new' => 'combatants#new'
   post 'combatants' => 'combatants#create'
